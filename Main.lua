be83c99cf15db88888c3cd6690b42772ed00f9fd
doadin <tbkizle@gmail.com>
2024-03-18 17:30:42 -0400

    Format Changes Update Print

--- Main.lua
+++ Main.lua
@@ -1,36 +1,36 @@
-﻿local addonName, addonTable = ...;
-_G[addonName] = addonTable;
+﻿local addonName, addonTable = ...
+_G[addonName] = addonTable
 
 --- @type MaxDps
 if not MaxDps then return end
-local MaxDps = MaxDps;
-local UnitPower = UnitPower;
+local MaxDps = MaxDps
+local UnitPower = UnitPower
 
-local Druid = MaxDps:NewModule('Druid', 'AceEvent-3.0');
-addonTable.Druid = Druid;
+local Druid = MaxDps:NewModule('Druid', 'AceEvent-3.0')
+addonTable.Druid = Druid
 
 Druid.spellMeta = {
 	__index = function(t, k)
-		print('Spell Key ' .. k .. ' not found!');
+		print('Spell Key ' .. k .. ' not found!')
 	end
 }
 
 function Druid:Enable()
-	Druid:UnregisterEvent('UNIT_SPELLCAST_SUCCEEDED');
+	Druid:UnregisterEvent('UNIT_SPELLCAST_SUCCEEDED')
 	if MaxDps.Spec == 1 then
-		MaxDps.NextSpell = Druid.Balance;
-		MaxDps:Print(MaxDps.Colors.Info .. 'Druid Balance');
+		MaxDps.NextSpell = Druid.Balance
+		MaxDps:Print(MaxDps.Colors.Info .. 'Druid Balance', "info")
 	elseif MaxDps.Spec == 2 then
-		MaxDps.NextSpell = Druid.Feral;
-		MaxDps:Print(MaxDps.Colors.Info .. 'Druid Feral');
-		Druid:RegisterEvent('UNIT_SPELLCAST_SUCCEEDED');
+		MaxDps.NextSpell = Druid.Feral
+		MaxDps:Print(MaxDps.Colors.Info .. 'Druid Feral', "info")
+		Druid:RegisterEvent('UNIT_SPELLCAST_SUCCEEDED')
 	elseif MaxDps.Spec == 3 then
-		MaxDps.NextSpell = Druid.Guardian;
-		MaxDps:Print(MaxDps.Colors.Info .. 'Druid Guardian');
+		MaxDps.NextSpell = Druid.Guardian
+		MaxDps:Print(MaxDps.Colors.Info .. 'Druid Guardian', "info")
 	elseif MaxDps.Spec == 4 then
-		MaxDps.NextSpell = Druid.Restoration;
-		MaxDps:Print(MaxDps.Colors.Info .. 'Druid Restoration');
+		MaxDps.NextSpell = Druid.Restoration
+		MaxDps:Print(MaxDps.Colors.Info .. 'Druid Restoration', "info")
 	end
 
-	return true;
+	return true
 end
