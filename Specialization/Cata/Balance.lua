3c587618711d9b76f1dedaf519b3e3e0c40795fb
tbkizle <tbkizle@gmail.com>
2025-01-14 09:56:56 -0500

    Update Cata Balance

--- Specialization/Cata/Balance.lua
+++ Specialization/Cata/Balance.lua
@@ -25,6 +25,7 @@ local RunesPT = Enum.PowerType.Runes
 local RunicPowerPT = Enum.PowerType.RunicPower
 local SoulShardsPT = Enum.PowerType.SoulShards
 local LunarPowerPT = Enum.PowerType.LunarPower
+local EclipsePowerPT = 26
 local HolyPowerPT = Enum.PowerType.HolyPower
 local MaelstromPT = Enum.PowerType.Maelstrom
 local ChiPT = Enum.PowerType.Chi
@@ -70,9 +71,13 @@ local ComboPointsDeficit
 local Mana
 local ManaMax
 local ManaDeficit
+local ManaPerc
 local Rage
 local RageMax
 local RageDeficit
+
+local eclipse_dir
+local eclipse
 
 local Balance = {}
 
@@ -94,7 +99,7 @@ function Balance:callaction()
     if (MaxDps:CheckSpellUsable(classtable.WildMushroomDetonate, 'WildMushroomDetonate')) and (buff[classtable.WildMushroomBuff].count == 3) and cooldown[classtable.WildMushroomDetonate].ready then
         if not setSpell then setSpell = classtable.WildMushroomDetonate end
     end
-    if (MaxDps:CheckSpellUsable(classtable.InsectSwarm, 'InsectSwarm')) and (( ticks_remain <2 or ( debuff[classtable.InsectSwarmDeBuff].remains <10 and buff[classtable.SolarEclipseBuff].up and eclipse <15 ) ) and ( buff[classtable.SolarEclipseBuff].up or buff[classtable.LunarEclipseBuff].up or timeInCombat <10 )) and cooldown[classtable.InsectSwarm].ready then
+    if (MaxDps:CheckSpellUsable(classtable.InsectSwarm, 'InsectSwarm')) and (( debuff[classtable.InsectSwarmDeBuff].duration <2 or ( debuff[classtable.InsectSwarmDeBuff].remains <10 and buff[classtable.SolarEclipseBuff].up and eclipse <15 ) ) and ( buff[classtable.SolarEclipseBuff].up or buff[classtable.LunarEclipseBuff].up or timeInCombat <10 )) and cooldown[classtable.InsectSwarm].ready then
         if not setSpell then setSpell = classtable.InsectSwarm end
     end
     if (MaxDps:CheckSpellUsable(classtable.WildMushroomDetonate, 'WildMushroomDetonate')) and (buff[classtable.WildMushroomBuff].count >0 and buff[classtable.SolarEclipseBuff].up) and cooldown[classtable.WildMushroomDetonate].ready then
@@ -103,13 +108,13 @@ function Balance:callaction()
     if (MaxDps:CheckSpellUsable(classtable.Typhoon, 'Typhoon')) and cooldown[classtable.Typhoon].ready then
         if not setSpell then setSpell = classtable.Typhoon end
     end
-    if (MaxDps:CheckSpellUsable(classtable.Starfall, 'Starfall')) and (eclipse <- 80) and cooldown[classtable.Starfall].ready then
+    if (MaxDps:CheckSpellUsable(classtable.Starfall, 'Starfall')) and (eclipse < -80) and cooldown[classtable.Starfall].ready then
         if not setSpell then setSpell = classtable.Starfall end
     end
-    if (MaxDps:CheckSpellUsable(classtable.Sunfire, 'Sunfire')) and (( ticks_remain <2 and not debuff[classtable.MoonfireDeBuff].duration >0 ) or ( eclipse <15 and debuff[classtable.SunfireDeBuff].remains <10 )) and cooldown[classtable.Sunfire].ready then
+    if (MaxDps:CheckSpellUsable(classtable.Sunfire, 'Sunfire')) and (( debuff[classtable.SunfireDeBuff].duration <2 and not debuff[classtable.MoonfireDeBuff].duration >0 ) or ( eclipse <15 and debuff[classtable.SunfireDeBuff].remains <10 )) and cooldown[classtable.Sunfire].ready then
         if not setSpell then setSpell = classtable.Sunfire end
     end
-    if (MaxDps:CheckSpellUsable(classtable.Moonfire, 'Moonfire')) and (buff[classtable.LunarEclipseBuff].up and ( ( ticks_remain <2 and not debuff[classtable.SunfireDeBuff].duration >0 ) or ( eclipse >- 20 and debuff[classtable.MoonfireDeBuff].remains <10 ) )) and cooldown[classtable.Moonfire].ready then
+    if (MaxDps:CheckSpellUsable(classtable.Moonfire, 'Moonfire')) and (buff[classtable.LunarEclipseBuff].up and ( ( debuff[classtable.MoonfireDeBuff].duration <2 and not debuff[classtable.SunfireDeBuff].duration >0 ) or ( eclipse >- 20 and debuff[classtable.MoonfireDeBuff].remains <10 ) )) and cooldown[classtable.Moonfire].ready then
         if not setSpell then setSpell = classtable.Moonfire end
     end
     if (MaxDps:CheckSpellUsable(classtable.Starsurge, 'Starsurge')) and (buff[classtable.SolarEclipseBuff].up or buff[classtable.LunarEclipseBuff].up) and cooldown[classtable.Starsurge].ready then
@@ -168,6 +173,7 @@ function Druid:Balance()
     Mana = UnitPower('player', ManaPT)
     ManaMax = UnitPowerMax('player', ManaPT)
     ManaDeficit = ManaMax - Mana
+    ManaPerc = (Mana / ManaMax) * 100
     targetHP = UnitHealth('target')
     targetmaxHP = UnitHealthMax('target')
     targethealthPerc = (targetHP >0 and targetmaxHP >0 and (targetHP / targetmaxHP) * 100) or 100
@@ -190,62 +196,31 @@ function Druid:Balance()
     AstralPower = UnitPower('player', LunarPowerPT)
     AstralPowerMax = UnitPowerMax('player', LunarPowerPT)
     AstralPowerDeficit = AstralPowerMax - AstralPower
+    eclipse = UnitPower('player', EclipsePowerPT)
     local currentSpell = fd.currentSpell
-    local wrathCount = GetSpellCount(classtable.Wrath)
-    local starfireCount = GetSpellCount(classtable.Starfire)
-    local origWrathCount = wrathCount
-    local origStarfireCount = starfireCount
-    classtable.Incarnation =  classtable.IncarnationChosenofElune
-    local CaInc = talents[classtable.Incarnation] and classtable.Incarnation or classtable.CelestialAlignment
-    local castingMoonSpell = false
-    if currentSpell == classtable.Wrath then
-    	AstralPower = AstralPower + 6
-    	wrathCount = wrathCount - 1
-    elseif currentSpell == classtable.Starfire then
-    	AstralPower = AstralPower + 8
-    	starfireCount = starfireCount - 1
-    elseif currentSpell == classtable.FuryOfElune then
-    	AstralPower = AstralPower + 40
-    elseif currentSpell == classtable.ForceOfNature then
-    	AstralPower = AstralPower + 20
-    elseif currentSpell == classtable.StellarFlare then
-    	AstralPower = AstralPower + 8
-    elseif currentSpell == classtable.NewMoon then
-    	AstralPower = AstralPower + 10
-    	castingMoonSpell = true
-    elseif currentSpell == classtable.HalfMoon then
-    	AstralPower = AstralPower + 20
-    	castingMoonSpell = true
-    elseif currentSpell == classtable.FullMoon then
-    	AstralPower = AstralPower + 40
-    	castingMoonSpell = true
-    end
-    fd.eclipseInLunar = buff[classtable.EclipseLunar].up or (origStarfireCount == 1 and currentSpell == classtable.Starfire)
-    fd.eclipseInSolar = buff[classtable.EclipseSolar].up or (origWrathCount == 1 and currentSpell == classtable.Wrath)
+    local last_eclipse = (buff[classtable.EclipseLunar].up and not buff[classtable.EclipseSolar].up and "Lunar") or (buff[classtable.EclipseSolar].up and not buff[classtable.EclipseLunar].up and "Solar") or (buff[classtable.EclipseSolar].up and buff[classtable.EclipseLunar].up and "Both") or "Solar"
+    eclipse_dir = (last_eclipse == "Lunar" and 1) or (last_eclipse == "Solar" and -1) or (last_eclipse == "Both" and 1) or 1
+
+    fd.eclipseInLunar = buff[classtable.EclipseLunar].up
+    fd.eclipseInSolar = buff[classtable.EclipseSolar].up
     fd.eclipseInAny = fd.eclipseInSolar or fd.eclipseInLunar
     fd.eclipseInBoth = fd.eclipseInSolar and fd.eclipseInLunar
-    fd.eclipseSolarNext = wrathCount > 0 and starfireCount <= 0
-    fd.eclipseLunarNext = wrathCount <= 0 and starfireCount > 0
-    fd.eclipseAnyNext = wrathCount > 0 and starfireCount > 0
-    fd.wrathCount = wrathCount
-    fd.starfireCount = starfireCount
-    classtable.HalfMoon = 274282
-    classtable.FullMoon = 274283
+
     --for spellId in pairs(MaxDps.Flags) do
     --    self.Flags[spellId] = false
     --    self:ClearGlowIndependent(spellId, spellId)
     --end
     classtable.bloodlust = 0
-    classtable.FaerieFireDeBuff = 0
-    classtable.SunderArmorDeBuff = 0
-    classtable.ExposeArmorDeBuff = 0
+    classtable.FaerieFireDeBuff = 91565
+    classtable.SunderArmorDeBuff = 16145
+    classtable.ExposeArmorDeBuff = 60842
     classtable.WildMushroomBuff = 0
-    classtable.InsectSwarmDeBuff = 0
-    classtable.SolarEclipseBuff = 0
-    classtable.LunarEclipseBuff = 0
+    classtable.InsectSwarmDeBuff = 83017
+    classtable.SolarEclipseBuff = 48517
+    classtable.LunarEclipseBuff = 48518
     classtable.MoonfireDeBuff = 164812
     classtable.SunfireDeBuff = 164815
-    classtable.ShootingStarsBuff = 0
+    classtable.ShootingStarsBuff = 93400
 
     local function debugg()
     end
