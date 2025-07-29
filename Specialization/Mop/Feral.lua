b70fa28bcb06f93d42ed4a611395ff8db06a60b6
tbkizle <tbkizle@gmail.com>
2025-07-29 09:42:14 -0400

    Update MoP Feral

--- Specialization/Mop/Feral.lua
+++ Specialization/Mop/Feral.lua
@@ -281,8 +281,8 @@ function Druid:Feral()
     EnergyRegen = GetPowerRegenForPowerType(Enum.PowerType.Energy)
     EnergyTimeToMax = EnergyDeficit / EnergyRegen
     EnergyPerc = (Energy / EnergyMax) * 100
-    ComboPoints = UnitPower('player', ComboPointsPT)
-    ComboPointsMax = UnitPowerMax('player', ComboPointsPT)
+    ComboPoints = GetComboPoints("player", "target")
+    ComboPointsMax = 5
     ComboPointsDeficit = ComboPointsMax - ComboPoints
     classtable.Incarnation =  classtable.IncarnationAvatarofAshamane
     classtable.MoonfireCat =  classtable.Moonfire
