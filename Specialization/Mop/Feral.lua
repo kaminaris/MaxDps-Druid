7aa2b3624985f25bf8a4ea781d8c6297d80a898c
tbkizle <tbkizle@gmail.com>
2025-08-01 20:10:55 -0400

    Update MoP Feral

--- Specialization/Mop/Feral.lua
+++ Specialization/Mop/Feral.lua
@@ -76,6 +76,7 @@ local ManaDeficit
 local Rage
 local RageMax
 local RageDeficit
+local EnergyPerc
 
 local Feral = {}
 
@@ -102,104 +103,83 @@ local function ClearCDs()
     MaxDps:GlowCooldown(classtable.TigersFury, false)
     MaxDps:GlowCooldown(classtable.Incarnation, false)
     MaxDps:GlowCooldown(classtable.SkullBashCat, false)
+    MaxDps:GlowCooldown(classtable.Berserk, false)
 end
+
+local buffedRip =  false
+local buffedRake = false
+local BuffedThrash = false
 
 function Feral:single()
     if (MaxDps:CheckSpellUsable(classtable.SkullBashCat, 'SkullBashCat')) and cooldown[classtable.SkullBashCat].ready then
         MaxDps:GlowCooldown(classtable.SkullBashCat, ( select(8,UnitCastingInfo('target')) ~= nil and not select(8,UnitCastingInfo('target')) or select(7,UnitChannelInfo('target')) ~= nil and not select(7,UnitChannelInfo('target'))) )
     end
-    if (MaxDps:CheckSpellUsable(classtable.SavageRoar, 'SavageRoar')) and (buff[classtable.SavageRoarBuff].remains <= 1 or not buff[classtable.SavageRoarBuff].up) and cooldown[classtable.SavageRoar].ready then
+    -- Savage Roar
+    if MaxDps:CheckSpellUsable(classtable.SavageRoar, 'SavageRoar') and
+       (not buff[classtable.SavageRoarBuff].up or buff[classtable.SavageRoarBuff].remains <= 1) and
+       cooldown[classtable.SavageRoar].ready then
         if not setSpell then setSpell = classtable.SavageRoar end
     end
-    --if (MaxDps:CheckSpellUsable(classtable.TolvirPotion, 'TolvirPotion')) and (MaxDps:Bloodlust(1) or ( targethealthPerc <= 25 and buff[classtable.BerserkBuff].up ) or ttd <= 40) and cooldown[classtable.TolvirPotion].ready then
-    --    if not setSpell then setSpell = classtable.TolvirPotion end
-    --end
-    if (MaxDps:CheckSpellUsable(classtable.TigersFury, 'TigersFury')) and (Energy <= 35 and ( not buff[classtable.OmenofClarityBuff].up )) and cooldown[classtable.TigersFury].ready then
-        MaxDps:GlowCooldown(classtable.TigersFury, cooldown[classtable.TigersFury].ready)
+    -- Tiger's Fury
+    if MaxDps:CheckSpellUsable(classtable.TigersFury, 'TigersFury') and
+       Energy <= 35 and
+       cooldown[classtable.TigersFury].ready then
+        MaxDps:GlowCooldown(classtable.TigersFury, true)
     end
-    if (MaxDps:CheckSpellUsable(classtable.Berserk, 'Berserk')) and (buff[classtable.TigersFuryBuff].up or ( ttd <25 and cooldown[classtable.TigersFury].remains >6 )) and cooldown[classtable.Berserk].ready then
-        if not setSpell then setSpell = classtable.Berserk end
+    -- Berserk
+    if MaxDps:CheckSpellUsable(classtable.Berserk, 'Berserk') and
+       (Energy >= 90 or buff[classtable.TigersFuryBuff].up) and
+       cooldown[classtable.Berserk].ready then
+        MaxDps:GlowCooldown(classtable.Berserk, true)
     end
-    if (MaxDps:CheckSpellUsable(classtable.Incarnation, 'Incarnation') and talents[classtable.Incarnation]) and (buff[classtable.BerserkBuff].up and (talents[classtable.Incarnation] and true or false)) and cooldown[classtable.Incarnation].ready then
-        MaxDps:GlowCooldown(classtable.Incarnation, cooldown[classtable.Incarnation].ready)
-    end
-    if (MaxDps:CheckSpellUsable(classtable.FaerieFire, 'FaerieFire')) and (debuff[classtable.WeakenedArmorDeBuff].count <3) and cooldown[classtable.FaerieFire].ready then
-        if not setSpell then setSpell = classtable.FaerieFire end
-    end
-    if (MaxDps:CheckSpellUsable(classtable.FerociousBite, 'FerociousBite')) and (ComboPoints >= 1 and debuff[classtable.RipDeBuff].up and debuff[classtable.RipDeBuff].remains <= 2.1 and targethealthPerc <= 60) and cooldown[classtable.FerociousBite].ready then
+    -- Ferocious Bite (execute)
+    if MaxDps:CheckSpellUsable(classtable.FerociousBite, 'FerociousBite') and
+       targethealthPerc < 25 and
+       debuff[classtable.RipDeBuff].up and
+       ComboPoints >= 5 and
+       cooldown[classtable.FerociousBite].ready then
         if not setSpell then setSpell = classtable.FerociousBite end
     end
-    if (MaxDps:CheckSpellUsable(classtable.FerociousBite, 'FerociousBite')) and (ComboPoints >= 5 and debuff[classtable.RipDeBuff].up and targethealthPerc <= 60) and cooldown[classtable.FerociousBite].ready then
-        if not setSpell then setSpell = classtable.FerociousBite end
-    end
-    if (MaxDps:CheckSpellUsable(classtable.Rip, 'Rip')) and (ComboPoints >= 5 and ttd >= 6 and debuff[classtable.RipDeBuff].remains <2.0 and ( buff[classtable.BerserkBuff].up or ( debuff[classtable.RipDeBuff].remains + 1.9 ) <= cooldown[classtable.TigersFury].remains )) and cooldown[classtable.Rip].ready then
+    -- Rip
+    if MaxDps:CheckSpellUsable(classtable.Rip, 'Rip') and
+       ComboPoints == 5 and
+       (not debuff[classtable.RipDeBuff].up or debuff[classtable.RipDeBuff].remains < 2) or
+       (ComboPoints == 5 and (buff[classtable.TigersFuryBuff].up or buff[classtable.SavageRoarBuff].up) and ( bleeds and bleeds[UnitGUID("target")] and not bleeds[UnitGUID("target")].rip.tigersFury and not bleeds[UnitGUID("target")].rip.savageRoar )) and
+       cooldown[classtable.Rip].ready then
         if not setSpell then setSpell = classtable.Rip end
     end
-    if (MaxDps:CheckSpellUsable(classtable.Ravage, 'Ravage')) and ((not UnitThreatSituation("player", "target") or UnitThreatSituation("player", "target") <= 1) and debuff[classtable.RipDeBuff].up and debuff[classtable.RipDeBuff].remains <= 4 and ComboPoints <5) and cooldown[classtable.Ravage].ready then
-        if not setSpell then setSpell = classtable.Ravage end
-    end
-    if (MaxDps:CheckSpellUsable(classtable.Shred, 'Shred')) and ((not UnitThreatSituation("player", "target") or UnitThreatSituation("player", "target") <= 1) and debuff[classtable.RipDeBuff].up and debuff[classtable.RipDeBuff].remains <= 4 and ComboPoints <5) and cooldown[classtable.Shred].ready then
-        if not setSpell then setSpell = classtable.Shred end
-    end
-    if (MaxDps:CheckSpellUsable(classtable.FerociousBite, 'FerociousBite')) and (ComboPoints >= 5 and debuff[classtable.RipDeBuff].remains >5.0 and buff[classtable.SavageRoarBuff].remains >= 3.0 and buff[classtable.BerserkBuff].up) and cooldown[classtable.FerociousBite].ready then
-        if not setSpell then setSpell = classtable.FerociousBite end
-    end
-    if (MaxDps:CheckSpellUsable(classtable.SavageRoar, 'SavageRoar')) and (ComboPoints >= 5 and ttd >= 8.5 and buff[classtable.SavageRoarBuff].remains <= debuff[classtable.RipDeBuff].remains) and cooldown[classtable.SavageRoar].ready then
-        if not setSpell then setSpell = classtable.SavageRoar end
-    end
-    if (MaxDps:CheckSpellUsable(classtable.Rake, 'Rake')) and (debuff[classtable.RakeDeBuff].remains <3) and cooldown[classtable.Rake].ready then
+    -- Rake
+    if MaxDps:CheckSpellUsable(classtable.Rake, 'Rake') and
+       (not debuff[classtable.RakeDeBuff].up or debuff[classtable.RakeDeBuff].remains < 3) or
+       ( (buff[classtable.TigersFuryBuff].up or buff[classtable.SavageRoarBuff].up) and ( bleeds and bleeds[UnitGUID("target")] and not bleeds[UnitGUID("target")].rake.tigersFury and not bleeds[UnitGUID("target")].rake.savageRoar )) and
+       cooldown[classtable.Rake].ready then
         if not setSpell then setSpell = classtable.Rake end
     end
-    if (MaxDps:CheckSpellUsable(classtable.ThrashCat, 'ThrashCat')) and (buff[classtable.OmenofClarityBuff].up and debuff[classtable.ThrashCatDeBuff].remains <3) and cooldown[classtable.ThrashCat].ready then
+    -- Thrash (Clearcasting / dot refresh logic)
+    if MaxDps:CheckSpellUsable(classtable.ThrashCat, 'ThrashCat') and
+       (not debuff[classtable.ThrashCatDeBuff].up or debuff[classtable.ThrashCatDeBuff].remains < 3) or
+       ( (buff[classtable.TigersFuryBuff].up or buff[classtable.SavageRoarBuff].up) and ( bleeds and bleeds[UnitGUID("target")] and not bleeds[UnitGUID("target")].thrash.tigersFury and not bleeds[UnitGUID("target")].thrash.savageRoar )) and
+       (buff[classtable.OmenofClarityBuff].up or Energy >= 50) and
+       cooldown[classtable.ThrashCat].ready then
         if not setSpell then setSpell = classtable.ThrashCat end
     end
-    if (MaxDps:CheckSpellUsable(classtable.Ravage, 'Ravage')) and (buff[classtable.OmenofClarityBuff].up and math.huge > 0 and targethealthPerc >60) and cooldown[classtable.Ravage].ready then
-        if not setSpell then setSpell = classtable.Ravage end
-    end
-    if (MaxDps:CheckSpellUsable(classtable.Shred, 'Shred')) and (not UnitThreatSituation("player", "target") or UnitThreatSituation("player", "target") <= 1 and buff[classtable.OmenofClarityBuff].up and math.huge > 0 and targethealthPerc >60) and cooldown[classtable.Shred].ready then
+    -- Shred (Clearcasting or Berserk)
+    if MaxDps:CheckSpellUsable(classtable.Shred, 'Shred') and
+       (not UnitThreatSituation("player", "target") or UnitThreatSituation("player", "target") <= 1) and
+       (buff[classtable.OmenofClarityBuff].up or buff[classtable.BerserkBuff].up) and
+       cooldown[classtable.Shred].ready then
         if not setSpell then setSpell = classtable.Shred end
     end
-    if (MaxDps:CheckSpellUsable(classtable.Rake, 'Rake')) and (buff[classtable.OmenofClarityBuff].up) and cooldown[classtable.Rake].ready then
-        if not setSpell then setSpell = classtable.Rake end
-    end
-    if (MaxDps:CheckSpellUsable(classtable.FerociousBite, 'FerociousBite')) and (( ttd <= 4 and ComboPoints >= 5 ) or ttd <= 1) and cooldown[classtable.FerociousBite].ready then
+    -- Ferocious Bite
+    if MaxDps:CheckSpellUsable(classtable.FerociousBite, 'FerociousBite') and
+       ComboPoints >= 5 and
+       cooldown[classtable.FerociousBite].ready then
         if not setSpell then setSpell = classtable.FerociousBite end
     end
-    if (MaxDps:CheckSpellUsable(classtable.FerociousBite, 'FerociousBite')) and (ComboPoints >= 5 and debuff[classtable.RipDeBuff].remains >= 6.0) and cooldown[classtable.FerociousBite].ready then
-        if not setSpell then setSpell = classtable.FerociousBite end
-    end
-    if (MaxDps:CheckSpellUsable(classtable.Ravage, 'Ravage')) and (( buff[classtable.TigersFuryBuff].up or buff[classtable.BerserkBuff].up )) and cooldown[classtable.Ravage].ready then
-        if not setSpell then setSpell = classtable.Ravage end
-    end
-    if (MaxDps:CheckSpellUsable(classtable.Ravage, 'Ravage')) and (( ( ComboPoints <5 and debuff[classtable.RipDeBuff].remains <3.0 ) or ( ComboPoints == 0 and buff[classtable.SavageRoarBuff].remains <2 ) )) and cooldown[classtable.Ravage].ready then
-        if not setSpell then setSpell = classtable.Ravage end
-    end
-    if (MaxDps:CheckSpellUsable(classtable.Ravage, 'Ravage')) and (cooldown[classtable.TigersFury].remains <= 3.0) and cooldown[classtable.Ravage].ready then
-        if not setSpell then setSpell = classtable.Ravage end
-    end
-    if (MaxDps:CheckSpellUsable(classtable.Ravage, 'Ravage')) and (ttd <= 8.5) and cooldown[classtable.Ravage].ready then
-        if not setSpell then setSpell = classtable.Ravage end
-    end
-    if (MaxDps:CheckSpellUsable(classtable.Ravage, 'Ravage')) and (EnergyTimeToMax <= 1.0) and cooldown[classtable.Ravage].ready then
-        if not setSpell then setSpell = classtable.Ravage end
-    end
-    if (MaxDps:CheckSpellUsable(classtable.Rake, 'Rake')) and (( buff[classtable.TigersFuryBuff].up or buff[classtable.BerserkBuff].up )) and cooldown[classtable.Rake].ready then
-        if not setSpell then setSpell = classtable.Rake end
-    end
-    if (MaxDps:CheckSpellUsable(classtable.Rake, 'Rake')) and (( ( ComboPoints <5 and debuff[classtable.RipDeBuff].remains <3.0 ) or ( ComboPoints == 0 and buff[classtable.SavageRoarBuff].remains <2 ) )) and cooldown[classtable.Rake].ready then
-        if not setSpell then setSpell = classtable.Rake end
-    end
-    if (MaxDps:CheckSpellUsable(classtable.Rake, 'Rake')) and (cooldown[classtable.TigersFury].remains <= 3.0) and cooldown[classtable.Rake].ready then
-        if not setSpell then setSpell = classtable.Rake end
-    end
-    if (MaxDps:CheckSpellUsable(classtable.Rake, 'Rake')) and (ttd <= 8.5) and cooldown[classtable.Rake].ready then
-        if not setSpell then setSpell = classtable.Rake end
-    end
-    if (MaxDps:CheckSpellUsable(classtable.Rake, 'Rake')) and (EnergyTimeToMax <= 1.0) and cooldown[classtable.Rake].ready then
-        if not setSpell then setSpell = classtable.Rake end
-    end
-    if (MaxDps:CheckSpellUsable(classtable.Treants, 'Treants')) and ((talents[classtable.ForceofNature] and true or false)) and cooldown[classtable.Treants].ready then
-        if not setSpell then setSpell = classtable.Treants end
+    -- Mangle (Fallback generator to avoid energy cap)
+    if MaxDps:CheckSpellUsable(classtable.Mangle, 'Mangle') and
+       cooldown[classtable.Mangle].ready then
+        if not setSpell then setSpell = classtable.Mangle end
     end
 end
 
@@ -325,3 +305,40 @@ function Druid:Feral()
     Feral:callaction()
     if setSpell then return setSpell end
 end
+
+local buffBleed = CreateFrame("Frame")
+buffBleed:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+local bleeds = {}  -- [targetGUID] = { rip = {}, rake = {}, thrash = {} }
+
+bleedTracker:SetScript("OnEvent", function(self, event)
+    local timestamp, subEvent, _, sourceGUID, _, _, _, destGUID, _, _, _, spellId = CombatLogGetCurrentEventInfo()
+
+    if sourceGUID ~= UnitGUID("player") then return end
+
+    -- Check if we cast a bleed
+    if subEvent == "SPELL_CAST_SUCCESS" then
+        local tigersFuryUp = AuraUtil.FindAuraByName("Tiger's Fury", "player")
+        local savageRoarUp = AuraUtil.FindAuraByName("Savage Roar", "player")
+
+        if spellId == classtable.Rip then
+            bleeds[destGUID] = bleeds[destGUID] or {}
+            bleeds[destGUID].rip = {
+                tigersFury = buff[classtable.TigersFuryBuff].up,
+                savageRoar = buff[classtable.SavageRoarBuff].up,
+            }
+        elseif spellId == classtable.Rake then
+            bleeds[destGUID] = bleeds[destGUID] or {}
+            bleeds[destGUID].rake = {
+                tigersFury = buff[classtable.TigersFuryBuff].up,
+                savageRoar = buff[classtable.SavageRoarBuff].up,
+            }
+        elseif spellId == classtable.ThrashCat then
+            bleeds[destGUID] = bleeds[destGUID] or {}
+            bleeds[destGUID].thrash = {
+                tigersFury = buff[classtable.TigersFuryBuff].up,
+                savageRoar = buff[classtable.SavageRoarBuff].up,
+            }
+        end
+    end
+end)
+
